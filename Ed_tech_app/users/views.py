8cb41a62bf89e7598817b193632f83f1162e793a
Sharvil Palvekar <121839160+GeekySalami@users.noreply.github.com>
2024-02-04 21:25:24 +0530

    Add files via upload
    logout requires POST so created a form

--- Ed_tech_app/users/views.py
+++ Ed_tech_app/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .form import UserRegisterForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here.
 
